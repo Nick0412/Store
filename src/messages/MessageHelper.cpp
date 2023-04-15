98c43044bcee8010a9e4d35b5579287b7496a804
Nicholas Draper <nickd2225@gmail.com>
2023-04-14 23:00:42 -0700

    Refactored message helper function for placing 32 bit values.

--- src/messages/MessageHelper.cpp
+++ src/messages/MessageHelper.cpp
@@ -1,5 +1,6 @@
 #include "messages/MessageHelper.h"
 #include <arpa/inet.h>
+#include "messages/Constants.h"
 
 namespace Messages::MessageHelper
 {
@@ -8,13 +9,8 @@ namespace Messages::MessageHelper
         // TODO: Check that the byte buffer is large enough
 
         // Convert to a known size as std::size_t is variable on systems.
-        std::uint32_t size_bytes_uint32 = static_cast<uint32_t>(size_bytes);
-        std::uint32_t size_bytes_network_order = htonl(size_bytes_uint32);
-
-        byte_buffer[0] = size_bytes_network_order & 0xFF;
-        byte_buffer[1] = (size_bytes_network_order >> 8) & 0xFF;
-        byte_buffer[2] = (size_bytes_network_order >> 16) & 0xFF;
-        byte_buffer[3] = (size_bytes_network_order >> 24) & 0xFF;
+        std::uint32_t size_bytes_converted = static_cast<uint32_t>(size_bytes);
+        place32BitValueInByteBuffer(byte_buffer, Messages::Constants::MESSAGE_SIZE_OFFSET, size_bytes_converted);
     }
 
     void placeMessageTypeInByteBuffer(Common::Types::ByteVector& byte_buffer, MessageType message_type)
@@ -22,11 +18,19 @@ namespace Messages::MessageHelper
         // TODO: Check that buffer is large enough to type bytes
 
         std::uint32_t message_type_converted = static_cast<std::uint32_t>(message_type);
-        std::uint32_t message_type_network_order = htonl(message_type_converted);
-
-        byte_buffer[4] = message_type_network_order & 0xFF;
-        byte_buffer[5] = (message_type_network_order >> 8) & 0xFF;
-        byte_buffer[6] = (message_type_network_order >> 16) & 0xFF;
-        byte_buffer[7] = (message_type_network_order >> 24) & 0xFF;
+        place32BitValueInByteBuffer(byte_buffer, Messages::Constants::MESSAGE_TYPE_OFFSET, message_type_converted);
     }
+
+    void place32BitValueInByteBuffer(Common::Types::ByteVector& byte_buffer, std::size_t offset, std::uint32_t value)
+    {
+        // TODO: Check for out of bounds and size of buffer
+
+        std::uint32_t value_network_order = htonl(value);
+
+        byte_buffer[offset] = value_network_order & 0xFF;
+        byte_buffer[offset + 1] = (value_network_order >> 8) & 0xFF;
+        byte_buffer[offset + 2] = (value_network_order >> 16) & 0xFF;
+        byte_buffer[offset + 3] = (value_network_order >> 24) & 0xFF;
+    }
+
 }
