dc21a334f0b3a5d4e19ba71b3f798bf97da42056
Vivitsu Maharaja <vivitsum@amazon.com>
2023-04-17 18:17:03 -0700

    Fix build after rebase

--- src/messages/MessageHelper.cpp
+++ src/messages/MessageHelper.cpp
@@ -1,7 +1,6 @@
 #include "messages/MessageHelper.h"
+#include "messages/Constants.h"
 #include "common/Platform.h"
-#include "messages/Constants.h"
-#include "messages/Constants.h"
 
 namespace Messages::MessageHelper
 {
@@ -12,8 +11,6 @@ namespace Messages::MessageHelper
         // Convert to a known size as std::size_t is variable on systems.
         std::uint32_t size_bytes_converted = static_cast<uint32_t>(size_bytes);
         place32BitValueInByteBuffer(byte_buffer, Messages::Constants::MESSAGE_SIZE_OFFSET, size_bytes_converted);
-        std::uint32_t size_bytes_converted = static_cast<uint32_t>(size_bytes);
-        place32BitValueInByteBuffer(byte_buffer, Messages::Constants::MESSAGE_SIZE_OFFSET, size_bytes_converted);
     }
 
     void placeMessageTypeInByteBuffer(Common::Types::ByteVector& byte_buffer, MessageType message_type)
@@ -21,13 +18,6 @@ namespace Messages::MessageHelper
         // TODO: Check that buffer is large enough to type bytes
 
         std::uint32_t message_type_converted = static_cast<std::uint32_t>(message_type);
-        std::uint32_t message_type_network_order = Common::Platform::NetworkByteOrderLong(message_type_converted);
-
-        byte_buffer[4] = message_type_network_order & 0xFF;
-        byte_buffer[5] = (message_type_network_order >> 8) & 0xFF;
-        byte_buffer[6] = (message_type_network_order >> 16) & 0xFF;
-        byte_buffer[7] = (message_type_network_order >> 24) & 0xFF;
-
         place32BitValueInByteBuffer(byte_buffer, Messages::Constants::MESSAGE_TYPE_OFFSET, message_type_converted);
     }
 
@@ -46,7 +36,6 @@ namespace Messages::MessageHelper
     void placeStringInByteBuffer(Common::Types::ByteVector& byte_buffer, std::size_t offset, std::string str)
     {
         auto offset_iterator = byte_buffer.begin() + offset;
-        
         std::copy(str.begin(), str.end(), offset_iterator);
     }
 
